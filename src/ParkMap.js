3ee47b86ffe5cd4398dfdc9955163811f7bc85e3
Adam M. Duff <fikosan@gmail.com>
2018-08-17 13:25:09 -0400

    feat: Map style has been updated

--- src/ParkMap.js
+++ src/ParkMap.js
@@ -13,6 +13,7 @@ class ParkMap extends Component {
         markerVisible: false
     }
     fetchParks = () => {
+        console.log(this)
         let parks = [];
         let counties = [];
         fetch("https://data.ny.gov/api/views/9uuk-x7vh/rows.json")
@@ -103,9 +104,146 @@ class ParkMap extends Component {
     }
     render() {
         const { locations, counties, selectedPark, activeMarker, foursquareData, placesData } = this.state;
+            // Style made by Aiziel Nazario and posted to snazzymaps.com
+    // https://snazzymaps.com/style/137900/green
+    const styles = [
+        {
+          featureType: "all",
+          elementType: "labels.text.fill",
+          stylers: [
+            {
+              color: "#ffffff"
+            }
+          ]
+        },
+        {
+          featureType: "all",
+          elementType: "labels.text.stroke",
+          stylers: [
+            {
+              visibility: "on"
+            },
+            {
+              color: "#3e606f"
+            },
+            {
+              weight: 2
+            },
+            {
+              gamma: 0.84
+            }
+          ]
+        },
+        {
+          featureType: "all",
+          elementType: "labels.icon",
+          stylers: [
+            {
+              visibility: "off"
+            }
+          ]
+        },
+        {
+          featureType: "administrative",
+          elementType: "geometry",
+          stylers: [
+            {
+              weight: 0.6
+            },
+            {
+              color: "#313536"
+            }
+          ]
+        },
+        {
+          featureType: "landscape",
+          elementType: "geometry",
+          stylers: [
+            {
+              color: "#44a688"
+            }
+          ]
+        },
+        {
+          featureType: "poi",
+          elementType: "geometry",
+          stylers: [
+            {
+              color: "#13876c"
+            }
+          ]
+        },
+        {
+          featureType: "poi.attraction",
+          elementType: "geometry.stroke",
+          stylers: [
+            {
+              color: "#f5e4e4"
+            },
+            {
+              visibility: "off"
+            }
+          ]
+        },
+        {
+          featureType: "poi.attraction",
+          elementType: "labels",
+          stylers: [
+            {
+              visibility: "on"
+            },
+            {
+              lightness: "14"
+            }
+          ]
+        },
+        {
+          featureType: "poi.park",
+          elementType: "geometry",
+          stylers: [
+            {
+              color: "#13876c"
+            },
+            {
+              visibility: "simplified"
+            }
+          ]
+        },
+        {
+          featureType: "road",
+          elementType: "geometry",
+          stylers: [
+            {
+              color: "#067372"
+            },
+            {
+              lightness: "-20"
+            }
+          ]
+        },
+        {
+          featureType: "transit",
+          elementType: "geometry",
+          stylers: [
+            {
+              color: "#357374"
+            }
+          ]
+        },
+        {
+          featureType: "water",
+          elementType: "geometry",
+          stylers: [
+            {
+              color: "#0c4174"
+            }
+          ]
+        }
+      ];
         return (
             <Map
                 className="map"
+                styles={styles}
                 google={this.props.google}
                 zoom={7}
                 initialCenter={{

50fd2fca16e14dd1cebee7a20d66502a2dfad5c7
Adam M. Duff <fikosan@gmail.com>
2018-08-22 21:23:43 -0400

    feat: Adding gh pages

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { HashRouter, Route } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <HashRouter>
+    <Route exact path='/' render={() => <App />} />
+  </HashRouter>,
+  document.getElementById("root")
+);
 registerServiceWorker();

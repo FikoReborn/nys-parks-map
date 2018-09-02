8160c9f84d58233caf55a281f850cd01568af40a
Adam M. Duff <fikosan@gmail.com>
2018-09-01 21:26:43 -0400

    refactor: Removed router functionality since app is single page only

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-  <HashRouter>
-    <Route exact path="/" render={() => <App />} />
-  </HashRouter>,
+  <App />,
   document.getElementById('root')
 );
 registerServiceWorker();
